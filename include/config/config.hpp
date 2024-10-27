ad4522594877838e14ba5f295f7877e227931b44
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-27 20:03:36 -0300

    update 202410272003

--- include/config/config.hpp
+++ include/config/config.hpp
@@ -44,7 +44,7 @@
     #define PAN_ID                  0x1234
     #define ADDRESS_SLAVE           0x6002
     #define MRF24_TRANSMITER_ENABLE
-    #define MRF24_RECEIVER_ENABLE
+    //#define MRF24_RECEIVER_ENABLE
     //#define ENABLE_INTERRUPT_MRF24
     
 #elif  defined(MODULE_IS_RX)
