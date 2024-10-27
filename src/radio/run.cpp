a73ea9d06174391caf88815b50c42107c8f6c5ed
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-27 19:54:04 -0300

    update 202410271954

--- src/radio/run.cpp
+++ src/radio/run.cpp
@@ -20,7 +20,7 @@ namespace RUN{
 void Run_t::start()
 {
         [[gnu::unused]]  bool flag{true};
-        //system("clear"); 
+        system("clear"); 
     
     try{
             auto zigbee { std::make_unique<MRF24J40::Radio_t>()};        // Inicializar hilos y ejecutar las clases en paralelo                 
