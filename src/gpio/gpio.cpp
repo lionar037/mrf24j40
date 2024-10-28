505c6c23c9bf4f2bebd5ad2b5d37db5f708e8a80
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-28 19:27:59 -0300

    update 202410281927

--- src/gpio/gpio.cpp
+++ src/gpio/gpio.cpp
@@ -1,4 +1,3 @@
-
 //////////////////////////////////////////////////////////////////////////////
 //     
 //          filename            :   gpio.cpp
@@ -16,134 +15,274 @@
 //////////////////////////////////////////////////////////////////////////////
 
 #include <gpio/gpio.hpp>
+#include <config/config.hpp>
+
+#include <fstream>
+#include <string>
 #include <iostream>
-#include <stdio.h>
-//#include <signal.h>
-#include <csignal>
+#include <thread>
+#include <chrono>
+#include <filesystem>
+#include <unistd.h> 
+#include <cstring>
 
-namespace GPIO{
-
-//volatile bool keep_running = true;
-volatile int interrupt_count = 0;
-volatile bool running = true;
-
-
-    // Funci      n para manejar la se      al SIGINT (Ctrl+C)
-    void sigint_handler(int sig) {
-        running = false;
-    }
-
-    Gpio_t::Gpio_t()
-    {
-        #ifdef DBG_GPIO
-            std::printf("Gpio_t::Gpio_t()\n");
-            std::cout<<"pin led : " <<std::to_string(RPI_V2_GPIO_P1_32)<<"\n";
-            std::cout<<"pin interrupt : " <<std::to_string(RPI_V2_GPIO_P1_16)<<"\n";
-        #endif        
-        led_state = false ;
-        init();  
-        toogle();       
-        signal(SIGINT, sigint_handler);       
-    }
-
-    void Gpio_t::init(){        
-        
-        #ifdef SPI_BCM2835
-            if (!bcm2835_init() || !bcm2835_spi_begin()) 
-        #else
-            if (!bcm2835_init()) 
-        #endif
-            {
-                //throw "Initialization failed. Are you running as root?";
-                throw std::runtime_error("Initialization failed. Are you running as root?");
-            }  
-                                                
-
-        #ifdef SPI_BCM2835
-        bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);//for mode SPI
-        bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);//for mode SPI
-        bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_8);//for mode SPI
-        bcm2835_spi_chipSelect(BCM2835_SPI_CS0);//for mode SPI
-        #endif
-        bcm2835_gpio_fsel( LED_OUT , BCM2835_GPIO_FSEL_OUTP );                
-        bcm2835_gpio_write( LED_OUT , LOW ); 
-            
-        // Configurar el pin GPIO 23 como entrada
-        bcm2835_gpio_fsel(INTERRUPT_PIN, BCM2835_GPIO_FSEL_INPT);
-
-        // Habilitar pull-up interno para evitar valores flotantes
-        bcm2835_gpio_set_pud(INTERRUPT_PIN, BCM2835_GPIO_PUD_UP);
-
-        // Limpia cualquier interrupci      n previa pendiente
-        bcm2835_gpio_set_eds(INTERRUPT_PIN);
-
-        // Configurar para detectar flanco descendente (falling edge)
-        bcm2835_gpio_fen(INTERRUPT_PIN);
-
-        #ifdef DBG_GPIO
-            std::cout<<"Esperando interrupción en el pin : "<< INTERRUPT_PIN <<" ... \n";
-        #endif
-    }
-
-    void Gpio_t::close(){
-    #ifdef SPI_BCM2835
-        bcm2835_spi_end();
-    #endif
-        bcm2835_close();  
-    }
-
-    Gpio_t::~Gpio_t(){
-        close();
-    }
-
-    void Gpio_t::toogle() {
-        bcm2835_gpio_write(LED_OUT, led_state ? HIGH : LOW);
-        led_state = !led_state;
-    }
-
-    void Gpio_t::transfer(const uint8_t cmd){
-     #ifdef SPI_BCM2835
-             bcm2835_spi_transfer(cmd);
-     #endif                
-    } 
-
-    void Gpio_t::delay_ms(const int64_t time){
-         bcm2835_delay(time);
-    }
-
-    void Gpio_t::check_status(){
-        while (bcm2835_gpio_lev(INTERRUPT_PIN) == HIGH) {
-            bcm2835_gpio_write(LED_OUT,LOW);
-            led_state=false;
-        }
-    }
-
-
-    bool Gpio_t::interrupt_handler() {
-        if (bcm2835_gpio_eds(INTERRUPT_PIN)) 
-        {
-            // Limpia el flag de evento
-            bcm2835_gpio_set_eds(INTERRUPT_PIN);
-
-            interrupt_count++;
-            std::cout << "Interrupcion recibida: Mensaje " << interrupt_count <<"\n";
-
-            if (interrupt_count >= 3) 
-                running = false; // Detener el programa despu      s de 3 interrupci
-            
-            return false;
-        }
-        
-        return true;
-    }
-
-    void Gpio_t::app(bool& flag){        
-        if(running){
-            flag =interrupt_handler();
-            // Peque      o retraso para evitar consumir demasiados recursos
-            bcm2835_delay(100);                        
-        }        
-    }
-
+extern "C"{
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <errno.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+    #include <poll.h>
 }
 
+
+namespace GPIO {
+
+    Gpio_t::Gpio_t(bool& status)
+        : m_state(status), m_gpio_in_fd(-1) {
+        #ifdef DBG_GPIO
+        std::cout << "Gpio_t::Gpio_t()\n";
+        #endif
+    }
+
+    Gpio_t::~Gpio_t() {
+        CloseGpios();
+        #ifdef DBG_GPIO
+        std::cout << "~Gpio()\n";
+        #endif
+    }
+
+    int Gpio_t::file_open_and_write_value(std::string_view fname, std::string_view wdata) {
+        std::ofstream file(fname.data(), std::ios::out | std::ios::trunc);
+        if (!file.is_open()) {
+            #ifdef DBG_GPIO
+            std::cerr << "El archivo no existe o no se pudo abrir : " << fname << std::endl;
+            #endif
+            return -1;
+        }
+        file << wdata;
+        return 0;
+    }
+
+    int Gpio_t::gpio_export(int gpio_num) {
+
+        #ifdef DBG_GPIO
+        std::cout << "gpio_export()\n";
+        #endif
+        //std::string str = SYSFS_GPIO_PATH.data()  SYSFS_GPIO_EXPORT_FN.data() ;
+         std::string path = std::string(SYSFS_GPIO_PATH) + std::string(SYSFS_GPIO_EXPORT_FN); 
+        return file_open_and_write_value( path  , std::to_string(gpio_num));
+    }
+
+    int Gpio_t::gpio_unexport(int gpio_num) {
+        #ifdef DBG_GPIO
+        std::cout << "gpio_unexport()\n";
+        #endif
+        std::string path = std::string(SYSFS_GPIO_PATH) + std::string(SYSFS_GPIO_EXPORT_FN); 
+        return file_open_and_write_value( path  , std::to_string(gpio_num));
+    }
+
+    int Gpio_t::gpio_set_direction(int gpio_num, std::string_view dir) {
+        #ifdef DBG_GPIO
+        std::cout << "gpio_set_direction() : " << dir << std::endl;
+        #endif
+        std::string path = std::string(SYSFS_GPIO_PATH) +"/gpio" +  std::to_string(gpio_num) + std::string(SYSFS_GPIO_DIRECTION); 
+        return file_open_and_write_value( path, dir);
+    }
+
+    int Gpio_t::gpio_set_value(int gpio_num, std::string_view value) {
+        #ifdef DBG_GPIO
+        std::cout << "gpio_set_value()\n";
+        #endif
+        std::string path = std::string (SYSFS_GPIO_PATH) +  "/gpio" + std::to_string(gpio_num) + std::string (SYSFS_GPIO_VALUE);
+        return file_open_and_write_value( path , value);
+    }
+
+    int Gpio_t::gpio_set_edge(int gpio_num, std::string_view edge) {
+        #ifdef DBG_GPIO
+        std::cout << "gpio_set_edge()\n";
+        #endif
+        std::string path = std::string (SYSFS_GPIO_PATH)  + "/gpio" + std::to_string(gpio_num) + std::string (SYSFS_GPIO_EDGE);
+        return file_open_and_write_value( path , edge);
+    }
+
+    int Gpio_t::gpio_get_fd_to_value(int gpio_num) {
+        #ifdef DBG_GPIO
+        std::cout << "gpio_get_fd_to_value()\n";
+        #endif
+        const std::string fname = "/sys/class/gpio/gpio" + std::to_string(gpio_num) + "/value";
+        if (!std::filesystem::exists(fname)) {
+            #ifdef DBG_GPIO
+            std::cerr << "El archivo " << fname << " no existe." << std::endl;
+            #endif
+            return -1;
+        }
+        std::ifstream file(fname);
+        if (!file.is_open()) {
+            #ifdef DBG_GPIO
+            std::cerr << "No se pudo abrir el archivo " << fname << " para leer." << std::endl;
+            #endif
+            return -1;
+        }
+        std::string contenido((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        #ifdef DBG_GPIO
+        std::cout << "Contenido actual del archivo " << fname << ":\n" << contenido << std::endl;
+        #endif
+        return std::stoi(contenido);
+    }
+
+    void Gpio_t::CloseGpios() {
+        if (m_filenameGpio.is_open()) m_filenameGpio.close();
+        if (m_gpio_in_fd != -1) {close(m_gpio_in_fd);}
+        gpio_set_value(m_gpio_out, VALUE_LOW);
+        gpio_unexport(m_gpio_out);
+        gpio_unexport(m_gpio_in);
+        #ifdef DBG_GPIO
+        std::cout << "Gpio_t::CloseGpios()" << std::endl;
+        #endif
+    }
+
+    int Gpio_t::digitalWrite(uint16_t pin, std::string_view st) {
+        #ifdef DBG_GPIO
+        std::cout << "digitalWrite : " << pin << " type : " << st << std::endl;
+        #endif
+        return gpio_set_value(pin, st);
+    }
+
+    int Gpio_t::pinMode(uint16_t number_gpio, std::string_view read_direction) {
+        #ifdef DBG_GPIO
+        std::cout << "pinMode : " << number_gpio << " Direction : " << read_direction << std::endl;
+        #endif
+        return gpio_set_direction(number_gpio, read_direction);
+    }
+
+    int Gpio_t::digitalRead(int gpio) {
+        #ifdef DBG_GPIO
+        std::cout << "digitalRead: " << gpio << std::endl;
+        #endif
+        return gpio_get_fd_to_value(gpio);
+    }
+
+    int Gpio_t::settings(int gpio, std::string_view direction, std::ifstream& filenameTmp) {
+        filenameTmp.open("/sys/class/gpio/gpio" + std::to_string(gpio) + "/direction");
+        if (!filenameTmp.is_open()) {
+            #ifdef DBG_GPIO
+            std::cerr << "No se pudo abrir el archivo de dirección para el GPIO " << gpio << std::endl;
+            #endif
+            return -1;
+        }
+        return 0;
+    }
+
+    int Gpio_t::getNextId() {
+        int max_id = -1;
+        #ifdef DBG_GPIO
+            std::cout << "Gpio_t::getNextId()" << std::endl;
+        #endif
+        for (const auto& gpioPtr : m_gpio_cfg) {  // Cambiamos a gpioPtr
+            if (gpioPtr->ID > max_id) {  // Cambiar a ->
+                max_id = gpioPtr->ID;  // Cambiar a ->
+            }
+        }
+        return max_id + 1;
+    }
+
+    void Gpio_t::updateGpioMaps() {
+
+        #ifdef DBG_GPIO
+            std::cout << "Gpio_t::updateGpioMaps()" << std::endl;
+        #endif
+        m_gpioById.clear();
+        m_gpioByPin.clear();
+        for (const auto& gpioPtr : m_gpio_cfg) {  // Cambiamos a gpioPtr
+            m_gpioById[gpioPtr->ID] = gpioPtr.get();  // Cambiar a ->
+            m_gpioByPin[gpioPtr->gpio] = gpioPtr.get();  // Cambiar a ->
+        }
+    }
+
+
+    void Gpio_t::printGpios() const {
+        #ifdef DBG_GPIO
+            std::cout << "Gpio_t::printGpios()" << std::endl;
+        #endif
+        for (const auto& gpioPtr : m_gpio_cfg) {  // Cambiamos el nombre de la variable a gpioPtr
+            const GpioConform_t& gpio = *gpioPtr;  // Desreferenciamos el puntero
+            std::cout << "ID: " << gpio.ID << ", GPIO: " << gpio.gpio << ", Direction: " << gpio.dir 
+                      << ", Edge: " << gpio.edge << ", Value: " << gpio.value << "\n";
+        }
+    }
+
+    void Gpio_t::addGpio(uint16_t gpio_pin, std::string dir, std::string edge, std::string value) {
+        #ifdef DBG_GPIO
+            std::cout << "Gpio_t::addGpio" << std::endl;
+        #endif
+
+        int id = getNextId();
+        auto gpio = std::make_unique<GpioConform_t>(id, gpio_pin, std::move(dir), std::move(edge), std::move(value), true);
+        m_gpio_cfg.push_back(std::move(gpio)); // Usa std::move aquí
+        m_gpioById[id] = m_gpio_cfg.back().get(); // Almacena el puntero a la última entrada
+        m_gpioByPin[gpio_pin] = m_gpio_cfg.back().get(); // Almacena el puntero a la última entrada
+    }
+
+
+    const bool Gpio_t::app(bool& flag) 
+    {
+        //const unsigned int gpio_out = OUT_INTERRUPT;//originalmente es unsigned 
+        //const int gpio_in = IN_INTERRUPT;
+        struct pollfd fdpoll;
+        int m_num_fdpoll { 1 };        
+        int m_looper { 0 };
+        char *buf[64];
+
+        #ifdef DBG_GPIO
+            printf("const bool Gpio_t::app()...%d\r\n",m_res);   
+        #endif         
+
+        settings( m_gpio_in  , DIR_IN  ,m_filenameGpio);
+        settings( m_gpio_out , DIR_OUT ,m_filenameGpio);
+        
+        gpio_set_edge (m_gpio_in,EDGE_FALLING);
+        gpio_set_value(m_gpio_out,VALUE_HIGH);
+
+          
+        m_gpio_in_fd = gpio_get_fd_to_value(m_gpio_in);
+        // We will wait for button press here for 10s or exit anyway
+        if(m_state==true)
+        {
+        while(m_looper<READING_STEPS) {
+            memset((void *)&fdpoll,0,sizeof(fdpoll));
+            fdpoll.fd = m_gpio_in_fd;
+            fdpoll.events = POLLPRI;
+            m_res = poll(&fdpoll,m_num_fdpoll,POLL_TIMEOUT);
+
+            if(m_res < 0) {
+                #ifdef DBG_GPIO
+                printf("Poll failed...%d\r\n",m_res);   
+                #endif         
+                }
+            if(m_res == 0) {
+                #ifdef DBG_GPIO
+                    std::cout<<"\nPoll success...timed out or received button press...\r\n";
+                #endif
+                }
+            if(fdpoll.revents & POLLPRI) {
+                lseek(fdpoll.fd, 0, SEEK_SET);
+                read(fdpoll.fd, buf, 64);
+                #ifdef DBG_GPIO
+                    std::cout<<"Standby reading msj mrf24j40...\n";
+                #endif
+                }
+            ++m_looper;
+            fflush(stdout);
+            }
+            //std::this_thread::sleep_for(std::chrono::milliseconds(50));   
+        }
+        else{            
+            gpio_set_value(m_gpio_out,VALUE_HIGH);
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));            
+        }    
+        gpio_set_value(m_gpio_out,VALUE_LOW);
+        return false;
+    }
+}
