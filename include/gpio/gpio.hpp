3a0030f75adc41ec098f466d6aa5187c4e8a4b33
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-27 21:13:29 -0300

    update 202410272113

--- include/gpio/gpio.hpp
+++ include/gpio/gpio.hpp
@@ -33,9 +33,9 @@ namespace GPIO{
 
     struct Gpio_t{
         public:
-            explicit    Gpio_t();
-            ~Gpio_t();//=default noexcept;        
-            void        app(bool& ) ;//;{return true;}
+            Gpio_t();
+            ~Gpio_t();
+            void        app(bool& ) ;
             void        transfer(uint8_t );
             void        delay(const int64_t);
             void        toogle(bool&);
@@ -43,6 +43,16 @@ namespace GPIO{
             void init();
             void close();
         private :
-            static bool led_state;
+            inline static bool led_state;
+        #if defined(__SIZEOF_POINTER__) && (__SIZEOF_POINTER__ == 4)
+            // Si es de 32 bits
+            constexpr int IN_INTERRUPT = RPI_V2_GPIO_P1_23;//RPI_V2_GPIO_P1_23
+            constexpr int OUT_INTERRUPT = RPI_V2_GPIO_P1_12;//RPI_V2_GPIO_P1_12
+        #else
+            //constexpr int IN_INTERRUPT = 535;//restarle 512
+            //constexpr int OUT_INTERRUPT = 524;//restarle 512
+            constexpr int IN_INTERRUPT = RPI_V2_GPIO_P1_23;
+            constexpr int OUT_INTERRUPT = RPI_V2_GPIO_P1_12;
+        #endif
     };
 }
