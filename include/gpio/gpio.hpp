3d9fc5292ff2d57287d82d1660ae9d6c6ffbb274
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-27 21:14:25 -0300

    update 202410272114

--- include/gpio/gpio.hpp
+++ include/gpio/gpio.hpp
@@ -20,15 +20,14 @@
 
 
 namespace GPIO{
+
 #if defined(__SIZEOF_POINTER__) && (__SIZEOF_POINTER__ == 4)
     // Si es de 32 bits
-    constexpr int IN_INTERRUPT = RPI_V2_GPIO_P1_23;//RPI_V2_GPIO_P1_23
-    constexpr int OUT_INTERRUPT = RPI_V2_GPIO_P1_12;//RPI_V2_GPIO_P1_12
+    //constexpr int IN_INTERRUPT = RPI_V2_GPIO_P1_23;//RPI_V2_GPIO_P1_23
+    //constexpr int OUT_INTERRUPT = RPI_V2_GPIO_P1_12;//RPI_V2_GPIO_P1_12
 #else
     //constexpr int IN_INTERRUPT = 535;//restarle 512
     //constexpr int OUT_INTERRUPT = 524;//restarle 512
-    constexpr int IN_INTERRUPT = RPI_V2_GPIO_P1_23;
-    constexpr int OUT_INTERRUPT = RPI_V2_GPIO_P1_12;
 #endif
 
     struct Gpio_t{
@@ -44,15 +43,8 @@ namespace GPIO{
             void close();
         private :
             inline static bool led_state;
-        #if defined(__SIZEOF_POINTER__) && (__SIZEOF_POINTER__ == 4)
-            // Si es de 32 bits
-            constexpr int IN_INTERRUPT = RPI_V2_GPIO_P1_23;//RPI_V2_GPIO_P1_23
-            constexpr int OUT_INTERRUPT = RPI_V2_GPIO_P1_12;//RPI_V2_GPIO_P1_12
-        #else
-            //constexpr int IN_INTERRUPT = 535;//restarle 512
-            //constexpr int OUT_INTERRUPT = 524;//restarle 512
             constexpr int IN_INTERRUPT = RPI_V2_GPIO_P1_23;
             constexpr int OUT_INTERRUPT = RPI_V2_GPIO_P1_12;
-        #endif
+
     };
 }
