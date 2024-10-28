d8d84a8d711ce8ba8ae24f825b9735f275696c08
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-28 00:58:14 -0300

    update 202410280058

--- src/radio/radio.cpp
+++ src/radio/radio.cpp
@@ -52,10 +52,11 @@ std::string msj_txt = {"MRF24J40 RX"};
     }
 
     const bool Radio_t::Run(void){         
-        gpio->app(m_flag); 
-        gpio->toogle();                                           
+        gpio->toogle(); 
+        gpio->app(m_flag);                                                   
         Start(m_flag);                
         interrupt_routine() ;  
+        gpio->toogle();
         return m_flag; 
     }
 
